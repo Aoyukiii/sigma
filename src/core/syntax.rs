ee1c9a174b83c6ff69923f1676737191253101db
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2025-10-16 00:36:52 +0800

    Add syntax.rs

---
+++ src/core/syntax.rs
@@ -0,0 +1,92 @@
+use std::ops::{Deref, DerefMut};
+
+use crate::core::{
+    lexer::{Lexer, Token, TokenKind, TokenStream},
+    operator::OpKind,
+    utils::Span,
+};
+
+#[derive(Debug)]
+pub struct Syntax {
+    pub kind: SyntaxKind,
+    pub span: Span,
+}
+
+impl Deref for Syntax {
+    type Target = SyntaxKind;
+    fn deref(&self) -> &Self::Target {
+        &self.kind
+    }
+}
+
+impl Syntax {
+    fn new(kind: SyntaxKind, span: Span) -> Self {
+        Self { kind, span }
+    }
+}
+
+impl DerefMut for Syntax {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.kind
+    }
+}
+
+#[derive(Debug)]
+pub enum SyntaxKind {
+    Ident(String),
+    AtomLiteral(String),
+    Annotated(Box<Annotated>),
+    Lambda(Box<Lambda>),
+    InfixOp(Box<InfixOp>),
+    Let(Box<Let>),
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    ExpectExpr,
+    BadToken { tok: String },
+}
+
+pub fn pratt_parse<'a>(tok_stream: &'a mut TokenStream<'a>) -> Result<Syntax, Vec<ParseError>> {
+    if let Some(next) = tok_stream.next() {
+        let Token { kind, span } = next;
+        let lhs = match kind {
+            TokenKind::Atom(atom) => Syntax::new(SyntaxKind::AtomLiteral(atom.to_string()), span),
+            t => {
+                return Err(vec![ParseError::BadToken {
+                    tok: "".to_string(),
+                }]);
+            }
+        };
+
+        todo!()
+    } else {
+        Err(vec![ParseError::ExpectExpr])
+    }
+}
+
+#[derive(Debug)]
+pub struct Annotated {
+    expr: Box<Syntax>,
+    type_expr: Box<Syntax>,
+}
+
+#[derive(Debug)]
+pub struct Lambda {
+    param: Box<Syntax>,
+    body: Box<Syntax>,
+}
+
+#[derive(Debug)]
+pub struct InfixOp {
+    op: OpKind,
+    lhs: Box<Syntax>,
+    rhs: Box<Syntax>,
+}
+
+#[derive(Debug)]
+pub struct Let {
+    var: Box<Syntax>,
+    value: Box<Syntax>,
+    body: Box<Syntax>,
+}
