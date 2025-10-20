712c2f02eb12e540cd068c9d6b7e39d2aff8411d
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2025-10-21 00:23:43 +0800

    Add `Display` and correct infix binding power

--- src/core/operator.rs
+++ src/core/operator.rs
@@ -1,9 +1,4 @@
-#[derive(Debug)]
-pub enum OpKind {
-    Prefix(Prefix),
-    Infix(Infix),
-    Postfix(Postfix),
-}
+use std::fmt::Display;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Prefix {
@@ -22,6 +17,16 @@ impl Prefix {
     }
 }
 
+impl Display for Prefix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Plus => write!(f, "+"),
+            Self::Neg => write!(f, "-"),
+            Self::Not => write!(f, "!"),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Infix {
     Add,
@@ -34,11 +39,21 @@ pub enum Infix {
 impl Infix {
     pub fn binding_power(&self) -> (u8, u8) {
         match &self {
-            Infix::Add => (0, 0),
-            Infix::Sub => (0, 0),
-            Infix::Mul => (0, 0),
-            Infix::Div => (0, 0),
-            Infix::Dot => (0, 0),
+            Infix::Add | Infix::Sub => (1, 2),
+            Infix::Mul | Infix::Div => (3, 4),
+            Infix::Dot => (6, 5),
+        }
+    }
+}
+
+impl Display for Infix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Add => write!(f, "+"),
+            Self::Sub => write!(f, "-"),
+            Self::Mul => write!(f, "*"),
+            Self::Div => write!(f, "/"),
+            Self::Dot => write!(f, "."),
         }
     }
 }
