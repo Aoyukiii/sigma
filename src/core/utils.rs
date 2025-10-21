248cd49ba02a460dcb988d93905c98b4ab55f8a7
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2025-10-21 16:29:30 +0800

    Add `PrettyContext` to output prettier structure

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,5 +1,5 @@
 use std::{
-    fmt::{Debug, Display},
+    fmt::{Debug, Display, Write},
     ops::Range,
 };
 
@@ -39,5 +39,53 @@ impl Display for Span {
             "{}",
             format!("{}..{}", self.start, self.end).to_string().yellow()
         )
+    }
+}
+
+pub struct PrettyContext<'a> {
+    indent: &'a str,
+    level: usize,
+}
+
+impl<'a> PrettyContext<'a> {
+    pub fn new() -> Self {
+        Self {
+            indent: "    ",
+            level: 0,
+        }
+    }
+
+    pub fn indented(&'a self) -> Self {
+        Self {
+            indent: self.indent,
+            level: self.level + 1,
+        }
+    }
+
+    pub fn write_indent(&mut self, writer: &mut impl Write) -> std::fmt::Result {
+        write!(writer, "{}", self.indent.repeat(self.level))
+    }
+
+    pub fn write_field(&mut self, writer: &mut impl Write, key: &str, value: &impl PrettyPrint) -> std::fmt::Result {
+        write!(writer, "{}: ", key)?;
+        value.pretty_print(self, writer)?;
+        write!(writer, ",")
+    }
+
+    pub fn write_field_ln(&mut self, writer: &mut impl Write, key: &str, value: &impl PrettyPrint) -> std::fmt::Result {
+        let mut ctx = self.indented();
+        ctx.write_indent(writer)?;
+        write!(writer, "{}: ", key)?;
+        value.pretty_print(&mut ctx, writer)?;
+        writeln!(writer, ",")
+    }
+    
+}
+
+pub trait PrettyPrint {
+    fn pretty_print(&self, ctx: &mut PrettyContext, w: &mut impl Write) -> std::fmt::Result;
+
+    fn print(&self, w: &mut impl Write) -> std::fmt::Result {
+        self.pretty_print(&mut PrettyContext::new(), w)
     }
 }
