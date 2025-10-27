a8061f7f8e2ab8ac34b1a364c11a2effca2983e6
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2025-10-27 23:37:48 +0800

    Add elaborated AST definition

--- src/core/syntax/ast/elaborated/expr.rs
+++ src/core/syntax/ast/elaborated/expr.rs
@@ -1,2 +1,36 @@
+use crate::core::{
+    syntax::ast::elaborated::id::{DebruijnId, ExprId},
+    utils::span::Span,
+};
+
 #[derive(Debug)]
-pub enum Expr {}
+pub struct Expr {
+    kind: ExprKind,
+    span: Span,
+    id: ExprId,
+}
+
+#[derive(Debug)]
+pub enum ExprKind {
+    Var(DebruijnId),
+    Atom,
+    Type,
+    AtomLiteral(String),
+    Lambda(Box<Expr>),
+    Application(Box<Application>),
+    Let(Box<Let>),
+    Error,
+}
+
+#[derive(Debug)]
+pub struct Let {
+    pub var: DebruijnId,
+    pub value: Box<Expr>,
+    pub body: Box<Expr>,
+}
+
+#[derive(Debug)]
+pub struct Application {
+    pub func: Box<Expr>,
+    pub arg: Box<Expr>,
+}
---
+++ src/core/syntax/ast/elaborated/id.rs
@@ -0,0 +1,5 @@
+#[derive(Debug)]
+pub struct ExprId(pub usize);
+
+#[derive(Debug)]
+pub struct DebruijnId(pub usize);
