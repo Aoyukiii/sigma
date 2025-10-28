cd032e8834484c1ad2e8d409793c21edfa583e29
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2025-10-29 00:05:14 +0800

    Add more id kind to id.rs

--- src/core/syntax/ast/elaborated/id.rs
+++ src/core/syntax/ast/elaborated/id.rs
@@ -1,5 +1,8 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct ExprId(pub usize);
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct DebruijnId(pub usize);
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct StringId(pub usize);
