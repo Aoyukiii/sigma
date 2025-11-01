31e3fffd6c80f95444cf32db3feb4f3f16e3f6c0
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2025-11-01 23:52:42 +0800

    🧪 test:

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,30 +1,35 @@
+use colored::Colorize;
 use pretty_fmt::{PrettyFmt, impl_display_for_pretty_fmt};
 use pretty_fmt_macros::PrettyFmt;
 
 #[derive(PrettyFmt)]
 enum Expr {
+    #[pretty_fmt("{}", arg0)]
     Number(f64),
-    #[pretty_fmt("UnitThing")]
+    #[header("U")]
     Unit,
 
+    #[header("{} ", op.to_string().yellow().bold())]
     InfixOp {
-        // #[skip]
+        #[skip]
         op: char,
         lhs: Box<Expr>,
         rhs: Box<Expr>,
     },
-
+    #[pretty_fmt("{}", arg0.with_ctx(ctx))]
     Big(Box<Big>),
 }
 
+impl_display_for_pretty_fmt!(Expr, Big);
+
 #[derive(PrettyFmt)]
+#[header("BIGGGG")]
 struct Big {
-    #[skip]
     a: i32,
     b: i32,
+    #[skip]
+    _meta: (),
 }
-
-impl_display_for_pretty_fmt!(Expr);
 
 fn main() {
     let e = Expr::InfixOp {
@@ -32,7 +37,11 @@ fn main() {
         lhs: Box::new(Expr::InfixOp {
             op: '+',
             lhs: Box::new(Expr::Unit),
-            rhs: Box::new(Expr::Number(2.)),
+            rhs: Box::new(Expr::Big(Box::new(Big {
+                a: 1,
+                b: 2,
+                _meta: (),
+            }))),
         }),
         rhs: Box::new(Expr::Number(2.)),
     };
