ff781919a5ab36a872199fd0e19ba89ed4bba543
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2025-11-01 14:13:15 +0800

    🧪 test(test): Add expanding test

---
+++ test/src/main.rs
@@ -0,0 +1,25 @@
+use pretty_fmt::{PrettyFmt, impl_display_for_pretty_fmt};
+use pretty_fmt_macros::PrettyFmt;
+
+#[derive(PrettyFmt)]
+enum Expr {
+    Number(f64),
+    #[pretty_fmt("UnitThing")]
+    Unit,
+    InfixOp(char, Box<Expr>, Box<Expr>),
+}
+
+impl_display_for_pretty_fmt!(Expr);
+
+fn main() {
+    let e = Expr::InfixOp(
+        '+',
+        Box::new(Expr::InfixOp(
+            '+',
+            Box::new(Expr::Unit),
+            Box::new(Expr::Number(2.)),
+        )),
+        Box::new(Expr::Number(2.)),
+    );
+    println!("{e}")
+}
